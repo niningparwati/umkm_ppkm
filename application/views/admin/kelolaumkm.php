88850cb01379921230987db4935f394255984284
farahfitriavidaa <farahfitriavidaa@gmail.com>
2020-07-01 10:52:52 +0700

    benerin bug admin

--- application/views/admin/kelolaumkm.php
+++ application/views/admin/kelolaumkm.php
@@ -73,9 +73,6 @@
                 <a href="<?= base_url()?>Admin/editUMKM/<?=$u->id_umkm ?>" class="btn btn-warning">
                       <div><i class="fa fa-fw fa-pencil"></i>Edit</div>
                 </a>
-                <a class="btn btn-danger" data-toggle="modal" href="#" data-target="#hapus<?=$u->id_umkm?>">
-                    <i class="fa fa-fw fa-trash"></i> Hapus
-                </a>
               </td>
             </tr>
           <?php endforeach; ?>
@@ -96,28 +93,3 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- ini bagian buka hapus -->
-<?php foreach ($umkm as $s) { ?>
-<div class="modal fade" id="hapus<?=$s->id_umkm?>" tabindex="-1" role="dialog" aria-hidden="true" aria-labelledby="ExampleModalLabel">
-<div class="modal-dialog" role="document">
- <div class="modal-content">
-   <div class="modal-header">
-     <h4 class="modal-title" id="ExampleModalLabel">Konfirmasi Hapus</h4>
-     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-       <span aria-hidden="true">&times;</span>
-     </button>
-   </div>
-   <div class="modal-body">
-     <p>Anda Yakin Akan Menghapus <?= $s->nama_umkm ?>?</p>
-   </div>
-   <div class="modal-footer justify-content-between">
-     <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Tidak</button>
-     <a href="<?= site_url()?>Admin/hapusUMKM/<?= $s->id_umkm ?>" class="btn btn-danger">Iya</a>
-   </div>
- </div>
- <!-- /.modal-content -->
-</div>
-<!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
-<?php } ?>
