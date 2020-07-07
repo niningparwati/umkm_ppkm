7f31afccc41a2bfc2d345189df13ce310d56512c
Rafata <rafatabahar@gmail.com>
2020-07-07 22:00:15 +0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -1,62 +1,56 @@
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href="#"><b>UMKM </b>Katalog</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-      <p class="login-box-msg">Login Sebagai UMKM atau Paguyuban</p>
-      <?= $this->session->flashdata('notif')?>
-      <form action="<?= $action ?>" method="post">
-       <!--  <div class="form-group">
-          <label>Login sebagai</label>
-          <select class="form-control" name="level">
-            <option value="UMKM">UMKM</option>
-            <option value="Paguyuban">Paguyuban</option>
-            <option value="Admin">Admin</option>
-          </select>
-        </div> -->
-        <div class="form-group has-feedback">
-          <input type="text" name="username" class="form-control" placeholder="Username" required oninvalid="this.setCustomValidity('Username belum diisi')" oninput="setCustomValidity('')">
-          <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-        </div>
-        <div class="form-group has-feedback">
-          <input type="password" name="password" class="form-control" placeholder="Password" required oninvalid="this.setCustomValidity('Passoword belum diisi')" oninput="setCustomValidity('')">
-          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        </div>
-        <div class="row">
-          <!-- /.col -->
-          <div class="col-xs-12">
-            <button type="submit" name="submit" class="btn btn-primary btn-block btn-flat">Login</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-
-      <center>
-        Belum Punya akun? <a href="<?= base_url()?>Register" class="text-center">Daftar</a>
-      </center>
-
-    </div>
-    <!-- /.login-box-body -->
+<?php $this->load->view('Admin/Head') ?>
+<body class="hold-transition login-page" style="background-image: url('<?php echo base_url()?>uploads/background.jpg');background-repeat: no-repeat;background-attachment: fixed;background-size: cover;">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><b>UMKM </b>PPKM</a>
   </div>
-  <!-- /.login-box -->
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p style="color:red"><?php echo $berita ?></p>
+    <p class="login-box-msg">Login Sebagai UMKM atau Admin</p>
+    <?= $this->session->flashdata('notif')?>
+    <form action="<?=base_url()?>LoginAU/login" method="post">
+      <div class="form-group has-feedback">
+        <input type="text" name="username" class="form-control" placeholder="Username" required oninvalid="this.setCustomValidity('Username belum diisi')" oninput="setCustomValidity('')">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" name="password" class="form-control" placeholder="Password" required oninvalid="this.setCustomValidity('Passoword belum diisi')" oninput="setCustomValidity('')">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <!-- /.col -->
+        <div class="col-xs-12">
+          <button type="submit" name="submit" class="btn btn-primary btn-block btn-flat">Login</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
 
-  <!-- jQuery 3 -->
-  <script src="<?php echo base_url() ?>asset/bower_components/jquery/dist/jquery.min.js"></script>
-  <!-- Bootstrap 3.3.7 -->
-  <script src="<?php echo base_url() ?>asset/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-  <!-- iCheck -->
-  <script src="<?php echo base_url() ?>asset/plugins/iCheck/icheck.min.js"></script>
-  <script>
-    $(function () {
-      $('input').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue',
-        increaseArea: '20%' /* optional */
-      });
+
+    <center>
+      Belum Punya akun? <a href="<?= base_url()?>Register" class="text-center">Daftar</a>
+    </center>
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+<script src="<?php echo base_url() ?>asset/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url() ?>asset/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url() ?>asset/plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
     });
-  </script>
+  });
+</script>
 </body>
 </html>
